2d837f54bc804fe1c7329bbddf0a7dd16da4ac55
pearsedarcy <pearse.darcy@outlook.com>
2024-11-14 15:46:45 +0100

    Add comments to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
@@ -9,6 +9,15 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "author", "created", "updated")
     search_fields = ("title", "body")
     prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("author",)
+    date_hierarchy = "created"
+    ordering = ("created",)
+    status = ("status",)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "author", "created", "updated")
+    search_fields = ("author", "body")
     raw_id_fields = ("author",)
     date_hierarchy = "created"
     ordering = ("created",)
