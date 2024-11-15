ce4d1d4d490e3015043080f68c2bf66fb7be49cb
pearsedarcy <pearse.darcy@outlook.com>
2024-11-15 17:50:22 +0100

    Fix Debug issue on prod and add logo

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import dj_database_url
 import environ
 env = environ.Env()
-env.read_env()  # Reads the .env file
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,15 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = "lsJCLOxI5bYa1yyLME8rqA" 
-
-
-
-#SECRET_KEY = env('SECRET_KEY')
-#print("SECRET_KEY from env:", env('SECRET_KEY'))
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['.herokuapp.com', '.pearse.dev', '127.0.0.1' ]
 
