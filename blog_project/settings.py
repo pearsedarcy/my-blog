a154adff2753d1a7006cfdb6c7f8f2a59fd83621
pearsedarcy <pearse.darcy@outlook.com>
2024-11-16 15:13:21 +0100

    Fix status/published error

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 ALLOWED_HOSTS = ['.herokuapp.com', '.pearse.dev', '127.0.0.1' ]
 
 
